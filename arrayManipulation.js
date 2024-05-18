7414303b0aada054aef527da7e5d2afc97a2a5e5
bennydan7 <baowusu-dankwah@st.ug.edu.gh>
2024-05-18 05:17:48 +0000

    exported the functions

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -5,7 +5,18 @@ function processArray(numbers) {
   return numbers.map((num) => (num % 2 === 0 ? num * num : num * 3));
 }
 
+// Function to format strings based on corresponding numbers
+function formatArrayStrings(strings, numbers) {
+  return strings.map((str, index) => {
+    return numbers[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase();
+  });
+}
+
+// Export the functions
+module.exports = { processArray, formatArrayStrings };
+
 // Example usage:
-let numbers = [1, 2, 3, 4, 5];
-let processedNumbers = processArray(numbers);
-console.log(processedNumbers); // [3, 4, 9, 16, 15]
+let strings = ["hello", "lads", "javascript", "is", "cool"];
+let processedNumbers = processArray([1, 2, 3, 4, 5]);
+let formattedStrings = formatArrayStrings(strings, processedNumbers);
+console.log(formattedStrings); // Output: ["hello", "LADS", "javascript", "IS", "cool"]
